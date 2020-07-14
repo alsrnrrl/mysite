394f51153d07435dfe87e34ee27185852f2d1e8e
alsrnrrl <alsrnrrl@naver.com>
2020-07-14 17:51:14 +0900

    민국기기긱기

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pq-02_ixtqro&9+@!(eq=_obnt3^0c7kgi1r_21&zc=v5brsau'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
