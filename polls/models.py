bda90a6a499c8196388280d2f26f104541e2178b
alsrnrrl <alsrnrrl@naver.com>
2020-07-15 09:50:40 +0900

    버전1.0

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+    return '%s / %s' % (self.question_text, self.pub_date)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -10,5 +12,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text +str(self.votes)
+        # return self.choice_text +str(self.votes)
         return '%s %d' % (self.choice_text, self.votes)
--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Question
 
 def index(request):
     q_list = Question.objects.order_by('pub_date')[:5]
     # str_list = [ q.question_text for q in q_list ]
     # html = ', '.join(str_list)
-#  return HttpResponse("Hello, world. You're at the polls index.")
- return render('request, index.html', {'latest_question_list': q_list})
 
+    # return HttpResponse(html)
+    return render(
+        request, 'polls/index.html',
+        { 'latest_question_list': q_list} )
 
+def detail(request, question_id): # 질문 상세 페이지
+    #                               pk
+    question = Question.objects.get(id=question_id)
 
- def detail(request, question_id): # 질문 상세 페이지
-     question = Question.object.get(id=question_id)
+    return render(
+        request, 'polls/detail.html', 
+        {'question': question})
      
 #  return HttpResponse("You're looking at question %s." % question_id)
 
