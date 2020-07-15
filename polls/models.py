cc82fe695f45798f0b8b05cb3fc807c2b2d77983
alsrnrrl <alsrnrrl@naver.com>
2020-07-15 10:02:51 +0900

    잘 돌아가라

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
-    return '%s / %s' % (self.question_text, self.pub_date)
+        return '%s / %s' % (self.question_text, self.pub_date)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
